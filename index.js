cd9701bc6c2b8b1737773ac967ff6a843b2f238e
luumanhquan1 <luumanhquan2001@gmail.com>
2021-12-09 14:34:59 +0700

    init base

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const e = require("express");
 const express = require("express");
 var http = require("http");
+const { decode } = require("./src/helper/jwt_token");
 const app = express();
 const port = process.env.PORT || 5000;
 var server = http.createServer(app);
@@ -7,21 +9,35 @@ var io = require("socket.io")(server);
 
 //middlewre
 app.use(express.json());
-var clients = {};
+var idUser = {};
+io.use((socket, next) => {
+  const token = socket.handshake.headers.token;
+  try {
+    const data = decode(token);
+    if (data.isSuccess) {
+      socket.id = data.id;
+      next();
+    } else {
+      socket.disconnect();
+    }
 
+  } catch (err) {
+    socket.disconnect();
+  }
+
+});
 io.on("connection", (socket) => {
   console.log("connetetd");
   console.log(socket.id, "has joined");
-  socket.on("signin", (id) => {
-    console.log(id);
-    clients[id] = socket;
-    console.log(clients);
-  });
+ 
   socket.on("message", (msg) => {
-    console.log(msg);
-    let targetId = msg.targetId;
-    if (clients[targetId]) clients[targetId].emit("message", msg);
+
+    const { idNguoiNhan, mess } = msg;
+console.log(idNguoiNhan,mess);
+  io.to(idNguoiNhan).emit('getMess',mess);
+
   });
+
 });
 
 server.listen(port, "0.0.0.0", () => {
